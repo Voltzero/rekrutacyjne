29e83fedf85f7885b6619c1b1af8eb18897813d5
Michał Przybysz <voltzero2@gmail.com>
2022-05-10 18:13:15 +0200

    test fix

--- tests/Feature/ProductControllerTest.php
+++ tests/Feature/ProductControllerTest.php
@@ -18,7 +18,8 @@ class ProductControllerTest extends TestCase
         $productData = array(
             'name' => 'Test Product',
             'quantity' => 100,
-            'price' => '99,99'
+            'price' => '99,99',
+            'code' => '$!#$!SADQWE'
         );
         $token = User::first()->createToken('Bearer')->plainTextToken;
 
@@ -38,7 +39,8 @@ class ProductControllerTest extends TestCase
         $response = $this->put("/api/product/1", [
             'name' => 'changed name',
             'quantity' => 122,
-            'price' => "12"
+            'price' => "12",
+            'code' => '123123ADS'
         ], [
             'HTTP_AUTHORIZATION' => "Bearer ${token}"
         ]);
